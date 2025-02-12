d25f1bae361b8f153c7251d572fee565f7ef4142
Soren Antebi <sea22@ic.ac.uk>
2025-02-12 15:18:26 +0100

    rm

---
+++ build/CMakeFiles/unet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unet.dir/unet.cpp.o"
+  "CMakeFiles/unet.dir/unet.cpp.o.d"
+  "unet"
+  "unet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/unet.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for unet.
+# This may be replaced when dependencies are built.
